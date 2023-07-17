93328c88fed9f746e6236f062437dee6de933070
Pablo Gutierrez <jpgutierrez07.pg@gmail.com>
2023-07-16 22:16:54 -0600

    2.0.1

--- header.php
+++ header.php
@@ -23,8 +23,8 @@
             --color-accent-2: #00543B;
         }
     </style>
-    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/avify.css?v=2.0.0">
-    <script type="module" src="<?php bloginfo('template_url'); ?>/assets/avify.js?v=2.0.0"></script>
+    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/avify.css?v=2.0.1">
+    <script type="module" src="<?php bloginfo('template_url'); ?>/assets/avify.js?v=2.0.1"></script>
     <title></title>
 </head>
 
